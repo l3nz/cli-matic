8df5ff6245feb7ae6c7702bd5134d4a842272b4e
lenz <lenz@lenzs-MacBook-Pro.local>
2018-04-24 14:51:49 +0200

    Some new presets

--- src/cli_matic/presets.clj
+++ src/cli_matic/presets.clj
@@ -10,12 +10,29 @@
   (Integer/parseInt s))
 
 
+(defn asDate [s]
+  (try
+    (.parse
+      (java.text.SimpleDateFormat. "yyyy-MM-dd") s)
+    (catch Throwable t
+      nil)))
+
+
+
 (def known-presets
-  {:int {:parse-fn parseInt
-         :placeholder "N"}
+  {:int    {:parse-fn    parseInt
+            :placeholder "N"}
+
+   :int-0  {:parse-fn    parseInt
+            :placeholder "N"
+            :default     0}
 
    :string {:placeholder "S"}
 
+   :yyyy-mm-dd
+           {:placeholder "YYYY-MM-DD"
+            :parse-fn    asDate
+            :validate    [some? "Must be a date in format YYYY-MM-DD"]}
 
    })
 
