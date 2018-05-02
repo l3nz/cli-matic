b62191062f9eebb00bef8c292dd02bd7edd4c8b2
Burin Choomnuan <agilecreativity@gmail.com>
2018-05-01 23:04:51 -0400

    Use widely used style guide adopted by community

--- project.clj
+++ project.clj
@@ -3,17 +3,13 @@
   :url "https://github.com/l3nz/cli-matic"
   :license {:name "Eclipse Public License, v2"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
-  :dependencies [
-		[org.clojure/clojure "1.9.0"]
-		[org.clojure/spec.alpha "0.1.143"]
-		[org.clojure/tools.cli "0.3.5"]
-                 [orchestra "2017.11.12-1"]
-  	]
-   :plugins [[lein-eftest "0.5.1"]
-             [jonase/eastwood "0.2.5"]
-             [lein-kibit "0.1.6"]]
-
-    :deploy-repositories [["clojars"  {:sign-releases false :url "https://clojars.org/repo"}]
-                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
-)
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/spec.alpha "0.1.143"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [orchestra "2017.11.12-1"]]
+  :plugins [[lein-eftest "0.5.1"]
+            [jonase/eastwood "0.2.5"]
+            [lein-kibit "0.1.6"]]
+  :deploy-repositories [["clojars"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]])
 
--- src/cli_matic/specs.clj
+++ src/cli_matic/specs.clj
@@ -1,8 +1,6 @@
 (ns cli-matic.specs
   (:require [clojure.spec.alpha :as s]))
 
-
-
 (s/def ::existing-string string?)
 
 (s/def ::climatic-errors #{:ERR-CFG
@@ -11,80 +9,73 @@
                            :ERR-PARMS-GLOBAL
                            :ERR-PARMS-SUBCMD})
 
-
-;
-; The way your process runs; will generate
-; a return val and some lines for stdout/stderr.
-;
-
+;; The way your process runs; will generate
+;; a return val and some lines for stdout/stderr.
 
 (s/def ::retval int?)
+
 (s/def ::status (s/or :oth #{:OK :EXCEPTION}
                       :err ::climatic-errors))
+
 (s/def ::help   (s/or :none nil?
                       :some #{:HELP-GLOBAL :HELP-SUBCMD}))
+
 (s/def ::subcmd (s/or :none ::existing-string
                       :nil  nil?))
-(s/def ::stderr (s/coll-of string?))
 
+(s/def ::stderr (s/coll-of string?))
 
 (s/def ::RV
   (s/keys :req-un [::retval ::status ::help ::subcmd ::stderr]))
 
+;; Cli-matic option definition
+(s/def ::option ::existing-string) ;; ex-string
 
-;
-; Cli-matic option definition
-;
-
-(s/def ::option ::existing-string) ; ex-string
 (s/def ::shortened ::existing-string)
+
 (s/def ::as ::existing-string)
+
 (s/def ::type #{:int :string :int-0 :yyyy-mm-dd})
+
 (s/def ::default some?)
 
 (s/def ::climatic-option
   (s/keys :req-un [::option  ::as  ::type]
           :opt-un [::shortened ::default]))
 
-;
-; Climatic configuration
-;
-
+;; Climatic configuration
 (s/def ::description ::existing-string)
+
 (s/def ::version ::existing-string)
+
 (s/def ::command ::existing-string)
 
 (s/def ::opts  (s/coll-of ::climatic-option))
+
 (s/def ::runs ifn?)
 
 (s/def ::app (s/keys :req-un [::command ::description ::version]))
+
 (s/def ::global-opts ::opts)
 
 (s/def ::a-command (s/keys :req-un [::command ::opts ::runs]))
+
 (s/def ::commands (s/coll-of ::a-command))
 
 (s/def ::climatic-cfg (s/keys :req-un [::app ::global-opts ::commands]))
 
-
-
-;
-; Parsing of options.
-;
-
+;; Parsing of options.
 (s/def ::subcommand (s/or :empty nil?
                           :some ::existing-string))
+
 (s/def ::subcommand-def (s/or :empty nil?
                               :some ::a-command))
+
 (s/def ::commandline map?) ;; contains :_arguments as vec
+
 (s/def ::parse-errors (s/or :oth #{:NONE :HELP-GLOBAL :HELP-SUBCMD}
                             :err ::climatic-errors))
 
-
 (s/def ::error-text string?)
 
-
 (s/def ::lineParseResult (s/keys :req-un [::subcommand ::subcommand-def ::commandline ::parse-errors ::error-text]))
-
-
-
-
