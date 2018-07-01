5e7fb991d387ea4a08f462e01ad4c8504ddf09b9
lenz <lenz@lenzs-MacBook-Pro.local>
2018-07-01 13:45:55 +0200

    Sample script runner

--- runScripts.sh
+++ runScripts.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 
+function runcmd {
+echo
+echo "--------------------------------------------------------"
+echo "--  $1"
+echo "--------------------------------------------------------"
+
+time $1
+
+}
+
+
+
 CMD="clj -i examples/toycalc.clj -m toycalc"
 
-$CMD -?
+runcmd "$CMD -?"
+runcmd "$CMD add -?"
+runcmd "$CMD --base 16 add -a 1 -b 254"
+#runcmd "export AA=10 && $CMD add -b 20"
+
 
