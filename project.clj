eaeea87800a987e82196567cfda8e4ace25f039e
Lenz <lenz@home>
2022-04-02 18:34:27 +0200

    ==== Ver 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cli-matic "0.4.3"
+(defproject cli-matic "0.5.0"
   :description "Compact [sub]command line parsing library, for Clojure"
   :url "https://github.com/l3nz/cli-matic"
   :license {:name "Eclipse Public License, v2"
